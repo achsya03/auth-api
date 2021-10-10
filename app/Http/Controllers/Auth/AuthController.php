52aa0dc27be2f2a42e1873548cbcad6658c71428
achsya03 <ach.sya03@gmail.com>
2021-10-10 17:30:13 +0700

    Update [Middleware,Kernel,Model,Controller]

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
